d60bbb8eaffa1951d3a6779ecc3cb9a145499f0a
HarisSamty <haris.razzaq.165@gmail.com>
2026-01-02 19:14:46 +0500

    update language

--- LectureMind-AI-main/speech-to-text.js
+++ LectureMind-AI-main/speech-to-text.js
@@ -30,7 +30,7 @@ document.addEventListener('DOMContentLoaded', function() {
         ['Català',          ['ca-ES']],
         ['Čeština',         ['cs-CZ']],
         ['Deutsch',         ['de-DE']],
-        ['English',         ['en-AU', 'Australia'], ['en-CA', 'Canada'], ['en-IN', 'India'], ['en-NZ', 'New Zealand'], ['en-ZA', 'South Africa'], ['en-GB', 'United Kingdom'], ['en-US', 'United States']],
+        ['English',         ['en-AU', 'Australia'], ['en-CA', 'Canada'], ['en-PK', 'Pakistan'], ['en-NZ', 'New Zealand'], ['en-ZA', 'South Africa'], ['en-GB', 'United Kingdom'], ['en-US', 'United States']],
         ['Español',         ['es-AR', 'Argentina'], ['es-BO', 'Bolivia'], ['es-CL', 'Chile'], ['es-CO', 'Colombia'], ['es-CR', 'Costa Rica'], ['es-EC', 'Ecuador'], ['es-SV', 'El Salvador'], ['es-ES', 'España'], ['es-US', 'Estados Unidos'], ['es-GT', 'Guatemala'], ['es-HN', 'Honduras'], ['es-MX', 'México'], ['es-NI', 'Nicaragua'], ['es-PA', 'Panamá'], ['es-PY', 'Paraguay'], ['es-PE', 'Perú'], ['es-PR', 'Puerto Rico'], ['es-DO', 'República Dominicana'], ['es-UY', 'Uruguay'], ['es-VE', 'Venezuela']],
         ['Euskara',         ['eu-ES']],
         ['Français',        ['fr-FR']],
